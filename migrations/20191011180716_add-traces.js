58ad86d3699f805575be028524ea39278a8347fc
Vitor George <vitor.george@gmail.com>
2019-10-15 11:29:58 +0100

    Add migration for traces table.

---
+++ migrations/20191011180716_add-traces.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('traces', function (table) {
+    table.string('id').primary();
+    table.string('description');
+    table.specificType('geometry', 'GEOGRAPHY(Linestring, 4326)');
+    table.specificType('timestamps', 'float[]');
+    table.float('length');
+    table.timestamp('recordedAt');
+    table.timestamp('uploadedAt').defaultTo(knex.fn.now());
+    table.timestamp('updatedAt').defaultTo(knex.fn.now());
+    table.integer('creatorId');
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('traces');
+};
