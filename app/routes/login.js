253dfb9e9461ccd04bfcc18f6206d0359a0eff52
Vitor George <vitor.george@gmail.com>
2019-10-16 13:00:56 +0100

    Remove /login/mobile route and redirect from param URL, if valid.

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,19 +1,28 @@
 import Boom from '@hapi/boom';
 import config from 'config';
-const mobileCallbackUrl = config.get('osmOAuth.mobileCallbackUrl');
+const allowedRedirectURLs = config.get('osmOAuth.allowedRedirectURLs');
 
 const handler = function (request, h) {
   const { isAuthenticated, credentials } = request.auth;
-
+  const redirectTo = credentials.query.redirect;
   if (!isAuthenticated) {
     return Boom.unauthorized('Could not authenticate at OpenStreetMap.');
   }
 
-  // Redirect based on the route
-  if (request.url.pathname === '/login/mobile') {
-    return h.redirect(
-      `${mobileCallbackUrl}?#accessToken=${credentials.accessToken}`
-    );
+  // If `redirect` is passed
+  if (redirectTo) {
+    // Check if redirect URL is part of authorized list
+    for (let i = 0; i < allowedRedirectURLs.length; i++) {
+      const url = allowedRedirectURLs[i];
+      if (redirectTo.indexOf(url) === 0) {
+        return h.redirect(
+          `${redirectTo}?accessToken=${credentials.accessToken}`
+        );
+      }
+    }
+
+    // Return error if invalid redirect URL.
+    return Boom.badRequest('Invalid redirect URL.');
   } else {
     return {
       profile: credentials.profile,
@@ -25,14 +34,6 @@ const handler = function (request, h) {
 module.exports = [
   {
     path: '/login',
-    method: ['GET', 'POST'],
-    options: {
-      auth: 'openstreetmap',
-      handler
-    }
-  },
-  {
-    path: '/login/mobile',
     method: ['GET', 'POST'],
     options: {
       auth: 'openstreetmap',
