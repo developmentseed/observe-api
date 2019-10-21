79cccde5d3e222c68bb46599f725cfcaf1a13b47
Vitor George <vitor.george@gmail.com>
2019-10-21 17:23:08 +0100

    Add GET /photos/{id}.

---
+++ app/routes/photos/get.js
@@ -0,0 +1,55 @@
+import config from 'config';
+import Boom from '@hapi/boom';
+import Joi from '@hapi/joi';
+import { getPhoto, photoToJson } from '../../models/photos';
+import logger from '../../services/logger';
+import db from '../../services/db';
+
+const idLength = config.get('idLength');
+
+/**
+ * @apiGroup Photos
+ *
+ * @api {get} /photos/:id 2. GET /photos/:id
+ * @apiDescription Get photo as JSON.
+ *
+ * @apiParam {string} id photo id.
+ *
+ * @apiUse Error4xx
+ */
+export default [
+  {
+    path: '/photos/{id}',
+    method: ['GET'],
+    options: {
+      auth: 'jwt',
+      validate: {
+        params: Joi.object({
+          id: Joi.string().length(idLength)
+        })
+      }
+    },
+    handler: async function (request, h) {
+      try {
+        const { id } = request.params;
+
+        const [photo] = await getPhoto(id, [
+          'id',
+          'bearing',
+          'ownerId',
+          'createdAt',
+          'uploadedAt',
+          'osmObjects',
+          db.raw('ST_AsGeoJSON(location) as location')
+        ]);
+
+        if (!photo) return Boom.notFound(`photo ${id} not found`);
+
+        return photoToJson(photo);
+      } catch (error) {
+        logger.error(error);
+        return Boom.badImplementation('Unexpected error.');
+      }
+    }
+  }
+];
--- test/test-photos.js
+++ test/test-photos.js
@@ -83,5 +83,80 @@ describe('Photos endpoints', async function () {
     });
   });
 
+  describe('GET /photos/{id}', function () {
+    it('return 401 for non-authenticated user', async function () {
+      try {
+        const client = new Client(apiUrl);
+        await client.get('/photos/abcdefghij');
+
+        // The test should never reach here, force execute catch block.
+        throw Error('An error was expected.');
+      } catch (error) {
+        // Check for the appropriate status response
+        expect(error.response.status).to.equal(401);
+      }
+    });
+
+    it('return 404 for non-existing photo', async function () {
+      try {
+        // Create client
+        const regularUser = await createMockUser();
+        const client = new Client(apiUrl);
+        await client.login(regularUser.osmId);
+
+        // Fetch resource
+        await client.get('/photos/abcdefghij');
+
+        // The test should never reach here, force execute catch block.
+        throw Error('An error was expected.');
+      } catch (error) {
+        // Check for the appropriate status response
+        expect(error.response.status).to.equal(404);
+      }
+    });
+
+    it('return 200 for existing photo', async function () {
+      // Create authenticated client
+      const regularUser = await createMockUser();
+      const client = new Client(apiUrl);
+      await client.login(regularUser.osmId);
+
+      // Get .jpg file as base64
+      const file = (await readFile(
+        path.join(__dirname, './fixtures/photo.jpg')
+      )).toString('base64');
+
+      // Set metadata data
+      const metadata = {
+        lon: 40,
+        lat: -13,
+        bearing: 272,
+        createdAt: new Date().toISOString(),
+        osmObjects: ['way/62239489', 'node/55555', 'relation/9999999']
+      };
+
+      // Post request
+      const {
+        data: { id }
+      } = await client.post('/photos', { file, ...metadata });
+
+      // Fetch resource
+      const { status, data } = await client.get(`/photos/${id}`);
+
+      expect(status).to.equal(200);
+
+      expect(data).to.have.property('id');
+      expect(data).to.have.property('uploadedAt');
+      expect(data).to.have.property('ownerId', regularUser.osmId);
+      expect(data).to.have.property('createdAt', metadata.createdAt);
+      expect(data).to.have.property('bearing', metadata.bearing);
+      expect(data.urls).to.deep.equal(getAllMediaUrls(id));
+      expect(data.osmObjects).to.deep.equal(metadata.osmObjects);
+      expect(data.location).to.deep.equal({
+        type: 'Point',
+        coordinates: [metadata.lon, metadata.lat]
+      });
+    });
+  });
 
 });
