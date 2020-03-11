996a67e73997579d27be85c62c25cb22957167d5
Sajjad Anwar <sajjadkm@gmail.com>
2020-03-11 15:45:40 +0530

    fix quadkey type and index, fix observation foreign keys

--- migrations/20200228172845_add-osm-objects.js
+++ migrations/20200228172845_add-osm-objects.js
@@ -1,15 +1,16 @@
 
-exports.up = function (knex) {
-  return knex.schema.createTable('osm_objects', function (table) {
+exports.up = async function (knex) {
+  await knex.schema.createTable('osm_objects', function (table) {
     table.string('id').notNullable();
     table.integer('version');
     table.primary(['id', 'version']);
     table.specificType('geom', 'GEOMETRY');
-    table.string('quadkey');
+    table.text('quadkey');
     table.jsonb('attributes');
     table.index('geom', 'GIST');
-    table.index('quadkey', 'varchar_pattern_ops');
   });
+
+  await knex.raw('CREATE INDEX txt_x on osm_objects (quadkey text_pattern_ops)');
 };
 
 exports.down = function (knex) {
--- migrations/20200228174527_observation-osm-object-fk.js
+++ migrations/20200228174527_observation-osm-object-fk.js
@@ -1,12 +1,12 @@
 
 exports.up = function (knex) {
   return knex.schema.table('observations', function (table) {
-    table.foreign(['osmObjectId', 'osmObjectVersion']).references(['osm_objects.id', 'osm_objects.version']);
+    table.foreign(['osmObjectId', 'osmObjectVersion']).references(['id', 'version']).on('osm_objects');
   });
 };
 
 exports.down = function (knex) {
   return knex.schema.table('observations', function (table) {
-    table.dropForeign('osmObject');
+    table.dropForeign(['osmObjectId', 'osmObjectVersion']);
   });
 };
