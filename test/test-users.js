c74252bbd74711946878f922d7903cfccc88226e
Sajjad Anwar <sajjadkm@gmail.com>
2020-03-13 22:17:06 +0530

    fix user tests

--- test/test-users.js
+++ test/test-users.js
@@ -86,6 +86,7 @@ describe('Users endpoints', function () {
         // Remove properties not covered by this test
         delete found.photos;
         delete found.traces;
+        delete found.observations;
 
         expect(expected).to.deep.equal(found);
       }
@@ -119,6 +120,7 @@ describe('Users endpoints', function () {
         // Remove properties not covered by this test
         delete found.photos;
         delete found.traces;
+        delete found.observations;
 
         expect(expected).to.deep.equal(found);
       }
@@ -153,6 +155,7 @@ describe('Users endpoints', function () {
         // Remove properties not covered by this test
         delete found.photos;
         delete found.traces;
+        delete found.observations;
 
         expect(expected).to.deep.equal(found);
       }
