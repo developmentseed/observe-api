9b2a9ad7dd549d9ed5dbabbc0aa00258bd1d7372
Vitor George <vitor.george@gmail.com>
2020-03-24 20:26:02 +0000

    Change survey schema, add GET /surveys route

--- app/models/surveys.js
+++ app/models/surveys.js
@@ -2,36 +2,47 @@ import db from '../services/db';
 import { getQuestionsLatest } from './questions';
 
 export async function createSurvey (data) {
-  const {
-    name,
-    ownerId,
-    optionalQuestions,
-    mandatoryQuestions
-  } = data;
+  const { name, ownerId, optionalQuestions, mandatoryQuestions } = data;
 
-  const [ id ] = await db('surveys').insert({
-    name,
-    ownerId,
-    optionalQuestions,
-    mandatoryQuestions
-  }, 'id');
+  const [id] = await db('surveys').insert(
+    {
+      name,
+      ownerId,
+      optionalQuestions,
+      mandatoryQuestions
+    },
+    'id'
+  );
 
   return id;
 }
 
+async function populateQuestions (survey) {
+  // Get latest question versions
+  survey.questions = await getQuestionsLatest(survey.questions);
+
+  // Flag optional questions
+  survey.questions = survey.questions.map(q => {
+    q.optional = survey.optionalQuestions.indexOf(q.id) > -1;
+    return q;
+  });
+
+  return survey;
+}
+
 export async function getSurvey (id) {
-  const survey = await db('surveys').select()
+  const survey = await db('surveys')
+    .select()
     .where('id', '=', id)
     .first();
 
-  if (survey.optionalQuestions) {
-    const optionalQuestions = await getQuestionsLatest(survey.optionalQuestions);
-    survey.optionalQuestions = optionalQuestions;
-  }
+  return populateQuestions(survey);
+}
 
-  if (survey.mandatoryQuestions) {
-    const mandatoryQuestions = await getQuestionsLatest(survey.mandatoryQuestions);
-    survey.mandatoryQuestions = mandatoryQuestions;
-  }
-  return survey;
+export async function getSurveys () {
+  const surveys = await db('surveys')
+    .select()
+    .map(populateQuestions);
+
+  return surveys;
 }
