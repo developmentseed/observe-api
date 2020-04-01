f246c650944985fb6dd115dc12a62deb7afa1122
Vitor George <vitor.george@gmail.com>
2020-04-01 11:47:51 +0100

    Preserve question order on surveys

--- app/models/questions.js
+++ app/models/questions.js
@@ -1,19 +1,18 @@
 import db from '../services/db';
+import find from 'lodash.find';
 
 export async function createQuestion (data) {
-  const {
-    version,
-    label,
-    type,
-    options
-  } = data;
+  const { version, label, type, options } = data;
 
-  const id = await db('questions').insert({
-    version,
-    label,
-    type,
-    options
-  }, ['id']);
+  const id = await db('questions').insert(
+    {
+      version,
+      label,
+      type,
+      options
+    },
+    ['id']
+  );
 
   return getQuestion(id[0].id, version);
 }
@@ -37,27 +36,50 @@ export async function getQuestion (id, version = 'latest') {
 }
 
 export async function getQuestionsLatest (ids) {
-  const questions = await db.raw('WITH latest_version AS (SELECT id, MAX(version) as version FROM questions WHERE id IN (??) GROUP BY id) SELECT latest_version.id, latest_version.version, questions."createdAt", questions.label, questions.type, questions.options from latest_version INNER JOIN questions ON questions.id=latest_version.id AND questions.version=latest_version.version;', [ids]);
+  const questions = await db.raw(`
+    WITH latest_version AS (
+        SELECT
+          id,
+          MAX(version) as version
+        FROM questions
+        WHERE
+          id IN (??)
+        GROUP BY
+          id
+      )
+    SELECT
+      latest_version.id,
+      latest_version.version,
+      questions."createdAt",
+      questions.label,
+      questions.type,
+      questions.options
+    from latest_version
+    INNER JOIN questions ON questions.id = latest_version.id
+      AND questions.version = latest_version.version;
+  `,
+  [ids]
+  );
 
-  return questions.rows;
+  // Preserve original question order from ids
+  return ids.map(id => {
+    return find(questions.rows, q => q.id === id);
+  });
 }
 
 export async function updateQuestion (data) {
-  const {
-    id,
-    version,
-    label,
-    type,
-    options
-  } = data;
+  const { id, version, label, type, options } = data;
 
-  const response = await db('questions').insert({
-    id,
-    version,
-    label,
-    type,
-    options
-  }, ['id']);
+  const response = await db('questions').insert(
+    {
+      id,
+      version,
+      label,
+      type,
+      options
+    },
+    ['id']
+  );
 
   return getQuestion(response[0].id, version);
 }
