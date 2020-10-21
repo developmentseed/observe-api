3731d6cca781551a0fc011c104f84c00228210fb
Marc Farra <marcfarra@gmail.com>
2020-10-21 15:45:10 +0300

    Remove oauthstrategy from config

--- app/services/auth/index.js
+++ app/services/auth/index.js
@@ -1,10 +1,8 @@
 import setupOAuth from './oauth';
 import { setupJwtAuth } from './jwt';
 
-const config = require('config');
-
 async function setupAuth (server) {
-  await setupOAuth(server, config.get('oauthStrategy'));
+  await setupOAuth(server);
   await setupJwtAuth(server);
 }
 
