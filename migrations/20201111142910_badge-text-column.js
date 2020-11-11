f00317a0b2b4915a3c26dd79b5b36c4a95909aad
Marc Farra <marcfarra@gmail.com>
2020-11-11 14:36:01 +0200

    Fix badge column type to text

---
+++ migrations/20201111142910_badge-text-column.js
@@ -0,0 +1,13 @@
+const { toReadableStream } = require("@hapi/wreck");
+
+exports.up = async function (knex) {
+  return knex.schema.alterTable('badges', table => {
+    table.text('image').alter();
+  })
+};
+
+exports.down = async function (knex) {
+  return knex.schema.alterTable('badges', table => {
+    table.binary('image').alter();
+  })
+};
